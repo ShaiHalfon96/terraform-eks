530aceac56522db3fdc002cc92b4ffdd27f8abd5
Shai Halfon <shai.halfon@payu.com>
2024-08-23 12:45:13 +0300

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "eks_cluster_name" {
-  value = aws_eks_cluster.name
+  value = module.aws_eks_cluster.name
 }
 
 output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.endpoint
+  value = module.aws_eks_cluster.endpoint
 }
 
 output "eks_cluster_certificate_authority_data" {
-  value = aws_eks_cluster.certificate_authority.0.data
+  value = module.aws_eks_cluster.certificate_authority.0.data
 }
 
 output "eks_cluster_id" {
-  value = aws_eks_cluster.id
+  value = module.aws_eks_cluster.id
 }
