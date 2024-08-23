045ba48a4431481eba29ebc7816400da57f238cb
Shai Halfon <shai.halfon@payu.com>
2024-08-23 13:05:32 +0300

    fix securit group rule set up

--- eks.tf
+++ eks.tf
@@ -19,18 +19,24 @@ resource "aws_security_group" "worker_group" {
   description = var.security_groups[count.index].description
   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port   = each.value.ingress[0].from_port
-    to_port     = each.value.ingress[0].to_port
-    protocol    = each.value.ingress[0].protocol
-    cidr_blocks = each.value.ingress[0].cidr_blocks
+  dynamic "ingress" {
+    for_each = var.security_groups[count.index].ingress
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
 
-  egress {
-    from_port   = each.value.egress[0].from_port
-    to_port     = each.value.egress[0].to_port
-    protocol    = each.value.egress[0].protocol
-    cidr_blocks = each.value.egress[0].cidr_blocks
+  dynamic "egress" {
+    for_each = var.security_groups[count.index].egress
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
   }
 
   tags = var.tags
